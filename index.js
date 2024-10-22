1080cfef1577daf5ed31807ef2393209cfd32b45
Lucie Volemanová <lucievoleman@gmail.com>
2024-10-22 18:57:29 +0200

    lekce5

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+const question = document.querySelector('.question');
+question.textContent = "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.";
+const userAnswer = prompt('Vaše odpověď:');
+const answerText = document.querySelector('.answer__text');
+answerText.textContent = userAnswer;
+const answerElement = document.querySelector('.answer');
+if (userAnswer.toLowerCase() === 'bell') {
+    answerElement.classList.add('answer--correct');
+    answerElement.classList.remove('answer--wrong');
+  } else {
+    answerElement.classList.add('answer--wrong');
+    answerElement.classList.remove('answer--correct');
+  }
